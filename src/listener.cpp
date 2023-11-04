768c4fcef4dc3abab207380262d81a0433da5340
armgits <126507842+armgits@users.noreply.github.com>
2023-11-04 16:41:12 +0000

    Updated callback function name and the message to be said

--- src/listener.cpp
+++ src/listener.cpp
@@ -27,7 +27,7 @@ public:
   : Node("normal_person")
   {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
-        "topic_flat_earth", 10, std::bind(&NormalPerson::topic_callback,
+        "topic_flat_earth", 10, std::bind(&NormalPerson::listen_callback,
          this, _1));
 
     publisher_ = this->create_publisher<std_msgs::msg::String>(
@@ -35,9 +35,15 @@ public:
   }
 
 private:
-  void topic_callback(const std_msgs::msg::String & msg) const
+  void listen_callback(const std_msgs::msg::String & msg) const
   {
     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+
+    auto reply = std_msgs::msg::String();
+    reply.data = "ALWAYS HAS BEEN...";
+
+    RCLCPP_INFO(this->get_logger(), "And I said: '%s'", reply.data.c_str());
+    publisher_->publish(reply);
   }
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
