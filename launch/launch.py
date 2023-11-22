74ccc7e0f046853d954fa2dd0b33fb880d0253d1
armgits <126507842+armgits@users.noreply.github.com>
2023-11-22 03:30:57 +0000

    Added rosbag record process with an argument

--- launch/launch.py
+++ launch/launch.py
@@ -1,7 +1,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.conditions import IfCondition
+
 
 def generate_launch_description():
     realization_value = LaunchConfiguration('realization')
@@ -17,6 +19,13 @@ def generate_launch_description():
         default_value='false',
         description="Should the second astronaut end up shooting the first \
                                                astronaut for a dramatic ending?"
+    )
+
+    record_bag_value = LaunchConfiguration('record_bag')
+    record_bag_arg = DeclareLaunchArgument(
+        'record_bag',
+        default_value='False',
+        description="Record messages on all topics to a rosbag?"
     )
 
     first_astronaut_node = Node(
@@ -39,9 +48,17 @@ def generate_launch_description():
         ]
     )
 
+    record_bag_process = ExecuteProcess(
+        cmd=['ros2', 'bag', 'record', '-a', '-o', 'output_bag'],
+        output='screen',
+        condition=IfCondition(PythonExpression([record_bag_value]))
+    )
+    
     return LaunchDescription([
         realization_arg,
         dramatic_end_arg,
+        record_bag_arg,
         first_astronaut_node,
-        second_astronaut_node
+        second_astronaut_node,
+        record_bag_process
     ])
